b8ff71b724a5471fb14f5915d406d386c5fc1990
BananaTheWise <BananaTheWise@users.noreply.github.com>
2025-11-17 05:41:30 +0200

    Implemented Some Functions: 90% LoginPresenter.kt 50% SignupPresenter.kt

--- app/src/main/java/com/fueians/medicationapp/presenter/Login/LoginPresenter.kt
+++ app/src/main/java/com/fueians/medicationapp/presenter/Login/LoginPresenter.kt
@@ -1,27 +1,28 @@
 
 package com.fueians.medicationapp.presenter.Login
 
+import com.fueians.medicationapp.model.services.AuthResponse
+
 // ############################################################
 // ######################### Temporary ########################
 // ####### UserRepository (Model not committed yet) ###########
 // ####### LoginView (View not implemented yet) ###############
 // ############################################################
 
-interface LoginView {
+class LoginView {
     fun showPopup(message: String) {}
     fun changeView(screen: String) {}
 }
 
-interface UserRepository {
-    fun findUserByEmail(email: String): User?
-    fun addUser(name: String, email: String, password: String)
+class AuthRepository {
+    suspend fun login(email: String, password: String): AuthResponse? {
+        return null
+    }
+
+    fun addUser(name: String, email: String, password: String){}
 }
 
-data class User(
-    val name: String,
-    val email: String,
-    val password: String
-)
+data class UserEntity(val name: String, val email: String)
 
 // ############################################################
 // ############################################################
@@ -29,53 +30,174 @@ data class User(
 
 interface LoginContract {
 
-    // Entry point
-    fun login(email: String, password: String) {}
-    fun SignUpBtn() {}
+    // Entry Point
+    fun Login(email: String, password: String) {}
+    fun signupBtn() {}
     fun loginWithGoogle(idToken: String) {}
     fun loginWithFacebook(accessToken: String) {}
 
-    // Steps
-    private fun checkEmail(email: String) {}
-    private fun checkPassword(email: String, password: String) {}
-    private fun changeViewToDashboard() {}
-
-    // Cases
-    private fun onEmailWrongSyntax() {}
-    private fun onEmailNotFound() {}
-    private fun onEmailEmpty() {}
-    private fun onEmailCorrect() {}
-
-    private fun onPasswordWrong() {}
-    private fun onPasswordEmpty() {}
-    private fun onPasswordCorrect() {}
-
-    private fun onContinueWithGoogle() {}
-    private fun onContinueWithFacebook() {}
-
-    private fun onAccountLocked() {}
-    private fun onDeviceBlocked() {}
-
-    // ===================================================
-    // ===============  Not Agreed Upon  =================
-    // ===================================================
-    // Helper
-    private fun decreaseTries() {}
-    private fun popup(message: String) {}
-    private fun changeView(screen: String) {}
-    // ===================================================
+//    // Steps
+//    private fun checkEmail(email: String) {}
+//    private fun checkPassword(email: String, password: String) {}
+//    private fun changeViewToDashboard() {}
+//
+//    // Cases
+//    private fun onEmailEmpty() {}
+//    private fun checkEmailSyntax(email: String):Boolean {}
+//    private fun onEmailWrongSyntax() {}
+//    private fun onEmailNotFound() {}
+//    private fun onPasswordWrong() {}
+//    private fun onPasswordEmpty() {}
+//    private fun onContinueWithGoogle() {}
+//    private fun onContinueWithFacebook() {}
+//    private fun onAccountLocked() {}
+//    private fun onDeviceBlocked() {}
+//
+//    // ===================================================
+//    // ===============  Not Agreed Upon  =================
+//    // ===================================================
+//    private fun popup(message: String) {}
+//    private fun changeView(screen: String) {}
+//    // ===================================================
 
 }
 
-class LoginPresenter (    private val userRepository: UserRepository,
-                          private val view: LoginView
-) : LoginContract {
-    override fun login(email: String, password: String) {
+class LoginPresenter : LoginContract {
+    private val Communicator = AuthRepository()
+    private val View = LoginView()
+
+    override fun Login(email: String, password: String) {
+        // Step 1
+        if (email.isEmpty()){
+            onEmailEmpty()
+            return
+        }
+
+        if (!checkEmailSyntax(email)) {
+            onEmailWrongSyntax()
+            return
+        }
+
+        //        checkEmail(email);
+
+        // Step 2
+        if (password.isEmpty()){
+            onPasswordEmpty()
+            return
+        }
+
+        if (!checkPassword(email,password)){
+            onPasswordWrong()
+            return
+        }
+
+        // Step 3
+        onPasswordCorrect(email,password)
 
 
     }
 
+    // ============================================================
+    // =============== Not Implemented In Repo ====================
+    //    private fun checkEmail(email: String) {
+    //
+    //    }
+    //    private fun onEmailNotFound() {
+    //
+    //    }
+    // =============== Not Implemented In Repo ====================
+    // ============================================================
+
+    override fun signupBtn() {
+        changeView("null");
+    }
+
+
+    private fun checkEmailSyntax(email: String) : Boolean {
+        val emailRegex = Regex(
+            "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+            RegexOption.IGNORE_CASE
+        )
+        return emailRegex.matches(email)
+    }
+    private fun checkPassword(email: String, password: String) : Boolean {
+//        Communicator.login(email,password) // should I use suspend ????
+    return true
+    }
+
+    // Cases
+    private fun onEmailWrongSyntax() {
+        popup("null")
+    }
+    private fun onEmailEmpty() {
+        popup("null")
+    }
+
+    // =====================================================
+    // =============== No Longer Needed ====================
+    //    private fun onEmailCorrect() {
+    //
+    //    }
+    // =============== No Longer Needed ====================
+    // =====================================================
+
+
+    private fun onPasswordWrong() {
+        popup("null")
+    }
+    private fun onPasswordEmpty() {
+        popup("null")
+    }
+    private fun onPasswordCorrect(email: String,password: String) {
+        changeView("null")
+    }
 
 
 
+    // ===================================================
+    // ===============  Not Agreed Upon  =================
+
+    private fun onAccountLocked() {}
+    private fun onDeviceBlocked() {}
+
+    private fun popup(message: String) {
+
+    }
+    private fun changeView(screen: String) {
+
+    }
+
+
+    // ===============  Not Agreed Upon  =================
+    // ===================================================
+
+
+
+    // ===================================================
+    // ================== Not Implemented Yet ============
+
+    override fun loginWithFacebook(accessToken: String) {
+        onContinueWithFacebook()
+    }
+
+    override fun loginWithGoogle(idToken: String) {
+        onContinueWithGoogle()
+    }
+
+    private fun onContinueWithGoogle() {
+
+    }
+
+    private fun onContinueWithFacebook() {
+
+    }
+
+    // ================== Not Implemented Yet ============
+    // ===================================================
+
+
+}
+
+private class test{
+    val test = LoginPresenter();
 }
--- app/src/main/java/com/fueians/medicationapp/presenter/Signup/SignupPresenter.kt
+++ app/src/main/java/com/fueians/medicationapp/presenter/Signup/SignupPresenter.kt
@@ -1,27 +1,29 @@
 
 package com.fueians.medicationapp.presenter.Signup
 
+import com.fueians.medicationapp.model.services.AuthResponse
+import com.fueians.medicationapp.presenter.Login.AuthRepository
+
 // ############################################################
 // ######################### Temporary ########################
-// ####### UserRepository (Model not committed yet) ###########
+// ####### UserRepository (Waiting For Confirmation) ##########
 // ####### LoginView (View not implemented yet) ###############
 // ############################################################
 
-interface SignupView {
+class SignupView {
     fun showPopup(message: String) {}
     fun changeView(screen: String) {}
 }
 
-interface UserRepository {
-    fun findUserByEmail(email: String): User?
-    fun addUser(name: String, email: String, password: String)
+class AuthRepository {
+    suspend fun signup(email: String, password: String): AuthResponse? {
+        return null
+    }
+
+    fun addUser(name: String, email: String, password: String){}
 }
 
-data class User(
-    val name: String,
-    val email: String,
-    val password: String
-)
+data class UserEntity(val name: String, val email: String)
 
 // ############################################################
 // ############################################################
@@ -29,63 +31,224 @@ data class User(
 
 interface SignupContract {
 
-    // ===================================================
-    // ================= Public Button Functions ========
-    // ===================================================
-    fun onSignUpBtnClicked(name: String, email: String, password: String, confirmPassword: String) {}
-    fun onContinueWithGoogleClicked(idToken: String?) {}
-    fun onContinueWithFacebookClicked(accessToken: String?) {}
+    // Entry Point
+    fun Signup(name: String, email: String, password: String, confirmPassword: String) {}
     fun onLoginBtnClicked() {}
+    fun signupWithGoogle(idToken: String) {}
+    fun signupWithFacebook(accessToken: String) {}
 
-    // ===================================================
-    // ================= Signup Logic ===================
-    // ===================================================
-    private fun signup(name: String, email: String, password: String, confirmPassword: String) {}
-    private fun signupWithGoogle(idToken: String) {}
-    private fun signupWithFacebook(accessToken: String) {}
+//    // Steps
+//    private fun checkEmail(email: String) {}
+//    private fun isStrongPassword(password: String) {}
+//    private fun checkConfirmPassword(password: String, confirmPassword: String) {}
+//    private fun createAccount(name: String, email: String, password: String) {}
+//
+//    // Cases
+//    private fun changeViewToLogin() {}
+//    private fun signup(name: String, email: String, password: String, confirmPassword: String) {}
+//    private fun signupWithGoogle(idToken: String) {}
+//    private fun signupWithFacebook(accessToken: String) {}
+//
+//    // Cases
+//    private fun onEmailEmpty() {}
+//    private fun onEmailWrongSyntax() {}
+//    private fun onEmailAlreadyExists() {}
+//    private fun onPasswordEmpty() {}
+//    private fun onPasswordWeak() {}
+//    private fun onConfirmPasswordEmpty() {}
+//    private fun onConfirmPasswordMismatch() {}
+//    private fun onContinueWithGoogle() {}
+//    private fun onContinueWithFacebook() {}
+//    private fun onDeviceBlocked() {}
+//
+//    // ===================================================
+//    // ================= Helpers =========================
+//    // ===================================================
+//    private fun popup(message: String) {}
+//    private fun changeView(screen: String) {}
+//    // ===================================================
 
-    // ===================================================
-    // ================= Steps ==========================
-    // ===================================================
-    private fun checkEmail(email: String) {}
-    private fun checkPassword(password: String) {}
-    private fun checkConfirmPassword(password: String, confirmPassword: String) {}
-    private fun createAccount(name: String, email: String, password: String) {}
-    private fun changeViewToLogin() {}
+}
+
+class SignupPresenter : SignupContract{
+    private val Communicator = AuthRepository()
+    private val View = SignupView()
+
+    override fun Signup(name: String, email: String, password: String, confirmPassword: String) {
+        // Step 1
+        if (email.isEmpty()){
+            onEmailEmpty()
+            return
+        }
+
+        if (!checkEmailSyntax(email)){
+            onEmailWrongSyntax()
+            return
+        }
+
+        //        checkEmail(email)
+
+        //Step 2
+        if (password.isEmpty()){
+            onPasswordEmpty()
+            return
+        }
+
+        if (!isStrongPassword(password)){
+            onPasswordWeak()
+            return
+        }
+
+        if (confirmPassword.isEmpty()){
+            onConfirmPasswordEmpty()
+            return
+        }
+
+        if (!onConfirmPasswordMatch(confirmPassword,password)){
+            onConfirmPasswordMismatch()
+            return
+        }
+
+        createAccount(email,password)
+
+    }
+
+    // ============================================================
+    // =============== Not Implemented In Repo ====================
+    //    private fun checkEmail(email: String) {
+    //
+    //    }
+    //    private fun onEmailNotFound() {
+    //
+    //    }
+    // =============== Not Implemented In Repo ====================
+    // ============================================================
+
+    override fun onLoginBtnClicked() {
+        changeView("null")
+    }
+
+
+    private fun checkEmailSyntax(email: String) : Boolean {
+        val emailRegex = Regex(
+            "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+            RegexOption.IGNORE_CASE
+        )
+        return emailRegex.matches(email)
+    }
+
+    fun isStrongPassword(password: String): Boolean {
+
+        if (password.length < 8)
+            return false
+
+        if (!password.contains(Regex("[A-Z]")))
+            return false
+
+        if (!password.contains(Regex("[a-z]")))
+            return false
+
+        if (!password.contains(Regex("\\d")))
+            return false
+
+        if (!password.contains(Regex("[@\$!%*?&]")))
+            return false
+
+        return true
+    }
+
+    private fun onConfirmPasswordMatch(confirmPassword: String, password: String): Boolean {
+        return confirmPassword == password
+    }
+
+    private fun createAccount(name: String, email: String, password: String) {
+
+    }
+    private fun createAccount( email: String, password: String) {
+
+    }
+
+    private fun signup(name: String, email: String, password: String, confirmPassword: String) {
+
+    }
+
+    private fun checkEmail(email: String) {
+
+    }
+
+    private fun checkConfirmPassword(password: String, confirmPassword: String) {
+
+    }
+    private fun changeViewToLogin() {
+
+    }
 
     // ===================================================
     // ================= Cases ==========================
     // ===================================================
-    private fun onEmailEmpty() {}
-    private fun onEmailWrongSyntax() {}
-    private fun onEmailAlreadyExists() {}
-    private fun onEmailValid() {}
+    private fun onEmailEmpty() {
 
-    private fun onPasswordEmpty() {}
-    private fun onPasswordWeak() {}
-    private fun onPasswordValid() {}
+    }
+    private fun onEmailWrongSyntax() {
 
-    private fun onConfirmPasswordEmpty() {}
-    private fun onConfirmPasswordMismatch() {}
-    private fun onConfirmPasswordMatch() {}
+    }
+    private fun onEmailAlreadyExists() {
 
-    private fun onDeviceBlocked() {}
+    }
+    private fun onEmailValid() {
+
+    }
+
+    private fun onPasswordEmpty() {
+
+    }
+    private fun onPasswordWeak() {
+
+    }
+    private fun onPasswordValid() {
+
+    }
+
+    private fun onConfirmPasswordEmpty() {
+
+    }
+    private fun onConfirmPasswordMismatch() {
+
+    }
+
+    private fun onDeviceBlocked() {
+
+    }
 
     // ===================================================
     // ================= Helpers ========================
     // ===================================================
-    private fun decreaseTries() {}
     private fun popup(message: String) {}
     private fun changeView(screen: String) {}
 
-}
-
-class SignupPresenter(
-    private val view: SignupView,
-    private val userRepository: UserRepository
-) {
 
 
+    // ===================================================
+    // ================== Not Implemented Yet ============
+
+    override fun signupWithFacebook(accessToken: String) {
+        onContinueWithFacebook()
+    }
+
+    override fun signupWithGoogle(idToken: String) {
+        onContinueWithGoogle()
+    }
+
+    private fun onContinueWithGoogle() {
+
+    }
+
+    private fun onContinueWithFacebook() {
+
+    }
+
+    // ================== Not Implemented Yet ============
+    // ===================================================
 
 }
 
